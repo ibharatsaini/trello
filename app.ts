3c2451e856596ea0f0aa57822f5a5530d140def6
ibharatsaini <sainib601@gmail.com>
2024-10-27 14:49:51 +0530

    allowe every cors

--- app.ts
+++ app.ts
@@ -16,22 +16,15 @@ const app = express();
 // app.use(cors());
 
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  /\.vercel\.app$/ // regex to match any subdomain on vercel.app
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   /\.vercel\.app$/ // regex to match any subdomain on vercel.app
+// ];
 
 app.use(cookieParser());
 app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin, like mobile apps or curl requests
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.some((allowedOrigin) => allowedOrigin instanceof RegExp ? allowedOrigin.test(origin) : allowedOrigin === origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error("Not allowed by CORS"));
-  },
-  credentials: true, 
+  origin: "*",
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "..", "frontend", "build")));  
