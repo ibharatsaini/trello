8b0c7abdaca877865aa14cc871f7d5d02a544aef
ibharatsaini <sainib601@gmail.com>
2024-10-27 13:47:16 +0530

    fixed axios env var

--- frontend/src/lib/configureAxios.ts
+++ frontend/src/lib/configureAxios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Check if running in production or development
-const {VITE_API_BASE_URL, NODE_ENV} = import.meta.env
-const BASE_URL =  NODE_ENV == 'production' ?  VITE_API_BASE_URL : 'http://localhost:8080/' 
+const {VITE_API_BASE_URL, VITE_NODE_ENV} = import.meta.env
+const BASE_URL =  VITE_NODE_ENV == 'production' ?  VITE_API_BASE_URL : 'http://localhost:8080/' 
 // const API_BASE_URL = 'http://localhost:8080/'
 console.log(import.meta.env)
 // Axios instance configuration
