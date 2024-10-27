bc0ae7749e2b0fb8d17edfbdb43ad00f6c12e071
ibharatsaini <sainib601@gmail.com>
2024-10-27 14:21:04 +0530

    debug axios base url

--- frontend/src/lib/configureAxios.ts
+++ frontend/src/lib/configureAxios.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 const {VITE_API_BASE_URL, VITE_NODE_ENV} = import.meta.env
 const BASE_URL =  VITE_NODE_ENV == 'production' ?  VITE_API_BASE_URL : 'http://localhost:8080/' 
 // const API_BASE_URL = 'http://localhost:8080/'
+console.log(BASE_URL  )
 console.log(import.meta.env)
 // Axios instance configuration
 const axiosInstance = axios.create({
